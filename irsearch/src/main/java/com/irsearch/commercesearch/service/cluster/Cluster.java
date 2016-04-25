71cee84206fbdbc632ed4f57d41010c926ec5c38
Wyatt Lee Chastain <wyatt.lee.chastain@gmail.com>
2016-04-24 22:22:32 -0500

    Cleanup

--- irsearch/src/main/java/com/irsearch/commercesearch/service/cluster/Cluster.java
+++ irsearch/src/main/java/com/irsearch/commercesearch/service/cluster/Cluster.java
@@ -156,9 +156,7 @@ public class Cluster {
             clusterEntity.setClusterNo(currentCluster);
             clusterEntities.add(clusterEntity);
             clusterSize.put(currentCluster, 1);
-
         }
-
 
         TreeMap<Double, SearchEntity> rankedResults = new TreeMap<>(Collections.reverseOrder());
 
@@ -174,17 +172,6 @@ public class Cluster {
 
         List<SearchEntity> finalResults = new ArrayList<>(rankedResults.values());
 
-//        for (ClusterEntity clusterEntity : clusterEntities) {
-//            clusterEntity.setSize(clusterSize.get(clusterEntity.getClusterNo()));
-//        }
-//
-//        HashMap<String, Double> newRanks = new HashMap<>();
-//        HashMap<String, SearchEntity> searchEntityHashMap = new HashMap<>();
-//
-//        HashMap<Integer, Integer> clustersInResults = new HashMap<>();
-//
-//        double originalSize = (double) results.size();
-
         SearchClusterResults searchClusterResults = new SearchClusterResults();
 
         searchClusterResults.setClusters(clusterEntities);
@@ -194,124 +181,6 @@ public class Cluster {
         searchClusterResults.setClusters(clusterEntities);
 
         return searchClusterResults;
-// http://bl.ocks.org/mbostock/4063269
-//        double rankCount = 1;
-//        double additionalItems = results.size() / 4.0;
-//
-//        for (SearchEntity result : results) {
-//
-//            searchEntityHashMap.put(result.getUrl(), result);
-//            /*
-//             * We want an initial rank score to be based on an ratio of the total result set size
-//             * for the returned results (~200) over the document's original rank in that result set.
-//             */
-//            double rankAdd = (originalSize / rankCount);
-//
-//            /*
-//             * If result is in newRanks, it was added for being close to a centroid
-//             * and we want to add the initial rank score to the existing score.
-//             */
-//            if (newRanks.containsKey(result.getUrl())) {
-//                rankAdd += newRanks.get(result.getUrl()) + (originalSize / rankCount);
-//            }
-//
-//            /*
-//             * If for some reason the url wasn't clustered, continue.
-//             */
-//            if (!clusterAssignments.containsKey(result.getUrl())) {
-//                continue;
-//            }
-//
-//            int assignment = clusterAssignments.get(result.getUrl())[0].intValue();
-//
-//            /*
-//             * We want the cluster score for the document to be equal to the
-//             * the best document (closest to the centroid)'s distance over the current url's distance.
-//             */
-//            rankAdd += (
-//                    bestDocuments.get(assignment).firstKey() /
-//                            clusterAssignments.get(result.getUrl())[1]
-//            );
-//            newRanks.put(result.getUrl(), rankAdd);
-//
-//
-//            /*
-//             * We want to count how many times each cluster shows up.
-//             */
-//            if (!clustersInResults.containsKey(assignment)) {
-//                clustersInResults.put(assignment, 0);
-//            }
-//            clustersInResults.put(assignment, clustersInResults.get(assignment) + 1);
-//
-//            if (!newRanks.containsKey(bestDocuments.get(assignment).firstEntry().getValue())) {
-//                newRanks.put(bestDocuments.get(assignment).firstEntry().getValue(), 1.0);
-//            }
-//
-//            newRanks.put(bestDocuments.get(assignment).firstEntry().getValue(),
-//                    newRanks.get(bestDocuments.get(assignment).firstEntry().getValue()) + rankAdd );
-//
-//
-//            rankCount++;
-//        }
-//
-//        System.out.println("Items ranked");
-//
-//        /*
-//         * Now we want to add X additional documents
-//         */
-//        for (Map.Entry<Integer, Integer> counts : clustersInResults.entrySet()) {
-//            int cluster = counts.getKey();
-//            double bestDistance = bestDocuments.get(cluster).firstKey();
-//            double total = (double) counts.getValue();
-//
-//            int docsToAdd = (int)(((total / originalSize) * additionalItems) + 0.5);
-//
-//            for (Map.Entry<Double, String> newDocs : bestDocuments.get(cluster).entrySet()) {
-//                if (!newRanks.containsKey(newDocs.getValue())) {
-//                    newRanks.put(newDocs.getValue(), 0.0);
-//                }
-//
-//                newRanks.put(newDocs.getValue(),
-//                        newRanks.get(newDocs.getValue()) + total * (newDocs.getKey() / bestDistance));
-//            }
-//        }
-//
-//
-//
-//        /*
-//         * Now to reorder them.
-//         */
-//        TreeMap<Double, SearchEntity> finalRankings = new TreeMap<Double, SearchEntity>();
-//        for (Map.Entry<String, Double> newRank : newRanks.entrySet()) {
-//            SearchEntity searchEntity = new SearchEntity();
-//            if (searchEntityHashMap.containsKey(newRank.getKey())) {
-//                // One of the original items, so we have the result.
-//                searchEntity = searchEntityHashMap.get(newRank.getKey());
-//            } else {
-//                // New result; look for result in index.  Need to add lookup
-//                searchEntity.setUrl(newRank.getKey());
-//            }
-//
-//            finalRankings.put(newRank.getValue(), searchEntity);
-//        }
-//
-//        SearchClusterResults searchClusterResults = new SearchClusterResults();
-//        List<SearchEntity> searchEntities = new ArrayList<SearchEntity>(finalRankings.values());
-//        List<ClusterEntity> clusterEntities = new ArrayList<ClusterEntity>();
-//
-//        for (int i = 0; i < clusterTitles.size(); i++) {
-//            if (!clusterTitles.containsKey(i)) continue;
-//
-//            clusterEntities.add(new ClusterEntity(i,
-//                    clusterTitles.get(i),
-//                    clustersInResults.get(i)));
-//        }
-//
-//        searchClusterResults.setClusters(clusterEntities);
-//        searchClusterResults.setResults(searchEntities);
-//
-//        System.out.println("Results returned");
-//        return searchClusterResults;
     }
 
     public void generateGraphJSONAll() {
