eb2f66f03b3ec79b17bd744f363700b337392089
Subhasis Dutta <subhasisdutta300887@gmail.com>
2016-05-30 21:41:16 -0700

    change API key

--- user-interface/server/config/config.js
+++ user-interface/server/config/config.js
@@ -5,15 +5,15 @@ module.exports = {
   development: {    
     rootPath: rootPath,
     port: process.env.PORT || 3030,
-    google_search_key: 'AIzaSyCq1hkDNgcwG2YLo1gtmBJgUROL94b5L1I',
-    google_search_cx:'017341476643384181453:sh3_hjw1xma',
+    google_search_key: 'AIzaSyBBAZQPv5hMP_QHE5A6v-g7luC25nfMPC4',
+    google_search_cx:'015316648851022853627:d-2vcb0f5l4',
     google_query_URL: 'https://www.googleapis.com/customsearch/v1?q=',
     query_result_max: 100,
     bing_account_key: 'hi5LIvAVghLrU2pq7b3VYYwcXt0H09uAcX49y8Ag1mo',
     bing_query_URL: 'https://api.datamarket.azure.com/Bing/Search/v1/Web',
-    search_query_ws : 'http://localhost:8989/commercesearch/api/search?query=',
-    query_expansion_ws: 'http://localhost:8989/commercesearch/api/queryexpansion?query=',
-    query_cluster_ws: 'http://localhost:8989/commercesearch/api/clustering?query='
+    search_query_ws : 'http://localhost:8989/api/search?query=',
+    query_expansion_ws: 'http://localhost:8989/api/queryexpansion?query=',
+    query_cluster_ws: 'http://localhost:8989/api/clustering?query='
   },
   production: {
     rootPath: rootPath,    
